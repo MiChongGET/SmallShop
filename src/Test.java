d9d5d0191b6506666ea62f4fb9609831bbb3c82b
MiChong <15646660232qq.com>
2017-09-14 19:41:33 +0800

    测试类

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * 功能测试类
+ *
+ * @author MiChong
+ *
+ *
+ */
+
+
+public class Test {
+    public static void main(String[] args) {
+
+        ShowPage.SHOW_PAGE.setShowPage();
+
+
+    }
+}
