3aad8480394a68cfca54d73c1e7079d1347387c1
MiChong <15646660232qq.com>
2017-09-14 19:41:18 +0800

    操作面板

---
+++ src/ShowPage.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * 操作面板
+ *
+ * @author MiChong
+ *
+ */
+
+public enum  ShowPage {
+
+    SHOW_PAGE;
+
+    public void setShowPage(){
+
+        System.out.println("******************电器商城********************");
+
+        System.out.println("               1、用户登录");
+        System.out.println("               2、用户注册");
+        System.out.println("               3、退出系统");
+
+        System.out.println("******************电器商城********************");
+
+    }
+}
